64cc47c2979e1f58525d879d138b4e0e511a295a
Max Peterson <max@incuna.com>
2012-10-12 10:39:29 +0100

    Django 1.4 compatibility.
    Restored feed url and moved category url.

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from models import Entry # relative import
-from feeds import WeblogEntryFeed
+from .models import Entry
+from .feeds import WeblogEntryFeed
 
 info_dict = {
     'queryset': Entry.objects.all(),
@@ -20,13 +20,13 @@ else:
 
 
 urlpatterns = patterns('',
-                       #url(r'^feed/$', WeblogEntryFeed(), name='news_feed'),
-                       # the date based generic view requires a day name, which is unnecessay unless you're posting more than one post a day., so we're using object detail instead.
-                       url(r'^(\d{4})/(\d{2})/(?P<slug>[\w-]+)/$', 'django.views.generic.list_detail.object_detail', dict(queryset=Entry.objects.all(), slug_field='slug'), name='news_detail'),
-                       #url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', info_dict, name='news_archive_day'),
-                       url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'django.views.generic.date_based.archive_month', dict(info_dict, month_format='%m'), name='news_archive_month'),
-                       url(r'^(?P<year>\d{4})/$', 'django.views.generic.date_based.archive_year', info_dict, name='news_archive_year'),
-                       url(r'^tags/(?P<tag>.*)/$', 'tagging.views.tagged_object_list', tagging_dict, name='news_archive_tagged',),
-                       url(r'^(?P<category>.*)/$', 'news.views.category_object_list', name='news_archive_category',),
-                       url(r'^/?$', 'django.views.generic.date_based.archive_index', info_dict_index, name='news_archive_index',),
-                      )
+        url(r'^feed/$', WeblogEntryFeed(), name='news_feed'),
+        # the date based generic view requires a day name, which is unnecessay unless you're posting more than one post a day., so we're using object detail instead.
+        url(r'^(\d{4})/(\d{2})/(?P<slug>[\w-]+)/$', 'django.views.generic.list_detail.object_detail', dict(queryset=Entry.objects.all(), slug_field='slug'), name='news_detail'),
+        #url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', info_dict, name='news_archive_day'),
+        url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'django.views.generic.date_based.archive_month', dict(info_dict, month_format='%m'), name='news_archive_month'),
+        url(r'^(?P<year>\d{4})/$', 'django.views.generic.date_based.archive_year', info_dict, name='news_archive_year'),
+        url(r'^tags/(?P<tag>.*)/$', 'tagging.views.tagged_object_list', tagging_dict, name='news_archive_tagged',),
+        url(r'^category/(?P<category>.*)/$', 'news.views.category_object_list', name='news_archive_category',),
+        url(r'^/?$', 'django.views.generic.date_based.archive_index', info_dict_index, name='news_archive_index',),
+        )
