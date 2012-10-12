1b9a9474d1da9b5c6592a1abd231d1f81c51a56e
Max Peterson <max@incuna.com>
2012-10-12 10:44:44 +0100

    Bumped version to 1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "incuna-news",
     packages = find_packages(),
     include_package_data=True,
-    version = "1.2",
+    version = "1.3",
     description = "Provides news.",
     author = "Incuna Ltd",
     author_email = "admin@incuna.com",
